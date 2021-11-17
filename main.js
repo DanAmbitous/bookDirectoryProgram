60edd5f1fa3e4fe7d3b0bd557b367dacf29dc2f8
Danial Azadpour <danazad2016@gmail.com>
2021-11-17 17:57:35 -0600

    Added mongoose db

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
+}
+
 const express = require("express")
 const app = express()
 const expressLayouts = require("express-ejs-layouts")
@@ -7,6 +11,14 @@ app.set("views", `${__dirname}/views`)
 app.set("layout", "layouts/layout") //sets the file (layout) within the folder (layouts) to the layout of the web app so that certain HTML wouldn't be repeated
 app.use(expressLayouts)
 app.use(express.static("public")) //Contains the public content e.g. js, css, images, etc.
+
+const mongoose = require("mongoose")
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+})
+const db = mongoose.connection
+db.on("error", (error) => console.log(error))
+db.once("open", () => console.log("Connected to Mongoose"))
 
 // Routers
 const defaultRouter = require("./routes/default")
