d850bb2dba00b6b2558cc717b9039d1f7678d691
Danial Azadpour <danazad2016@gmail.com>
2021-11-21 12:58:57 -0600

    Added error handling on not providing an input for name property

--- main.js
+++ main.js
@@ -11,12 +11,13 @@ app.set("views", `${__dirname}/views`)
 app.set("layout", "layouts/layout") //sets the file (layout) within the folder (layouts) to the layout of the web app so that certain HTML wouldn't be repeated
 app.use(expressLayouts)
 app.use(express.static("public")) //Contains the public content e.g. js, css, images, etc.
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: false }))
 
 const mongoose = require("mongoose")
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
 })
+
 const db = mongoose.connection
 db.on("error", (error) => console.log(error))
 db.once("open", () => console.log("Connected to Mongoose"))
