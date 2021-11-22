f14636c27b57c48bba88544c65c161b7097a9393
Danial Azadpour <danazad2016@gmail.com>
2021-11-22 11:25:42 -0600

    fix
    altered title to make it follow the conventions

--- routes/author.js
+++ routes/author.js
@@ -4,11 +4,27 @@ const router = express.Router()
 const Author = require("../models/authorModel")
 
 //get all authors
-router.get("/", (req, res) => {
-  res.render("author/index")
+router.get("/", async (req, res) => {
+  let searchOptions = {}
+
+  //Check to see the query has a value name="the name" (name is in the URL), null checking is for going into that url without via the menu bar
+  if (req.query.name != null && req.query.name.length > 0) {
+    searchOptions.name = new RegExp(req.query.name, "i")
+  }
+
+  try {
+    const author = await Author.find(searchOptions)
+
+    res.render("author/index", {
+      authors: author,
+      searchOptions: req.query,
+    })
+  } catch (error) {
+    res.redirect("/")
+  }
 })
 
-// get all new authors
+//get all new authors
 router.get("/new", (req, res) => {
   res.render("author/new", { author: new Author() })
 })
@@ -23,13 +39,14 @@ router.post("/", async (req, res) => {
 
   //Saves it to db
   try {
-    const newAuthor = await author.save()
-    console.log(newAuthor)
+    await author.save()
+
     res.redirect("author")
   } catch (error) {
     res.render("author/new", {
       author: author,
       errorMessage: `Error: ${error}`,
+      error: true,
     })
   }
 })
